d2e9fc7c588c6c5a0f3d46fff34161def9102ac5
Konrad <konrad.marzec007@gmail.com>
2023-12-12 10:14:03 +0100

    cleanup

--- apps/docs/src/components/SVG.tsx
+++ apps/docs/src/components/SVG.tsx
@@ -1,8 +1,6 @@
 import React, { useMemo } from 'react';
 import { mesh } from '../utils/mesh.utils';
 import { marchingSquares } from '../utils/contour.utils';
-// import Contour from './Contour';
-// import Values from './Values';
 import ContourSinglePath from './ContourSinglePath';
 import { DENSITY_MAP, FN_MAP, MeshType } from '../constants/mesh.constants';
 import { useScale, useScreenSize } from '../common-hooks';
@@ -16,23 +14,6 @@ function SVG({ fnId }: SettingsProps) {
   const [xScale, yScale, step] = useScale(fnId, width, height);
 
   const [grid, min, max] = useMemo(() => mesh(FN_MAP[fnId], xScale, yScale, step), [fnId, width, height]);
-
-  // const contours = useMemo(() => {
-  //   if (!grid?.length) {
-  //     return null;
-  //   }
-
-  //   let res = [];
-  //   let i = Math.floor(min - 1);
-
-  //   while (i < Math.ceil(max + 1)) {
-  //     const delta = (max - min) / 20;
-
-  //     res.push(<Contour key={i} contour={marchingSquares(grid, step, (i += delta))} />);
-  //   }
-
-  //   return res;
-  // }, [grid, min, max]);
 
   const contours = useMemo(() => {
     if (!grid?.length) {
@@ -52,21 +33,6 @@ function SVG({ fnId }: SettingsProps) {
 
   return (
     <svg className="w-screen h-screen" viewBox={`0 0 ${width} ${height}`}>
-      {/* <Values grid={grid} min={min} max={max} step={step} /> */}
-
-      {/* <defs>
-        <pattern id="grid" width={step} height={step} patternUnits="userSpaceOnUse">
-          <path
-            fill="none"
-            strokeWidth={0.5}
-            strokeOpacity={0.4}
-            stroke="currentColor"
-            d={`M ${step} 0 L 0 0 0 ${step}`}
-          />
-        </pattern>
-      </defs>
-      <rect width="100%" height="100%" fill="url(#grid)" /> */}
-
       {contours}
     </svg>
   );
